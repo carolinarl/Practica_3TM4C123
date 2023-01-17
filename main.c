e02a9ceb8cf014e13e90568c2b993f2f2d1bd4f9
carolinarl <carito.03ags@hotmail.com>
2023-01-16 23:07:40 -0600

    commit 2

--- main.c
+++ main.c
@@ -19,7 +19,47 @@ int main(void)
     //  Configurar_UART2(); //Andrea,Avila,Pamela,Paulina -fclk 50MHZ Baud-rate 57600
     //  Configurar_UART5(); //Hector,Cecilia,Carolina,Jozzafat -fclk 40MHZ Baud-rate 28800
 
-
+    //printChar(c);
+    //printChar(b);
+    //printString("\n");
+   // char *arr = readString(',');
+   // printString(&arr[0]);
+   /*
+    while(1)
+    {
+         c = readChar();
+         switch(c)
+         {
+             case 'r':
+                 //GPIODATA port F 662
+                 printChar('a');
+                 GPIOF->DATA = (1<<1);
+                 break;
+             case 'b':
+                 //GPIODATA port F 662
+                 printChar('b');
+                 GPIOF->DATA = (1<<2);
+                 break;
+             case 'g':
+                 //GPIODATA port F 662
+                 printChar('c');
+                 GPIOF->DATA = (1<<3);
+                 break;
+             case 'y':
+                 //GPIODATA port F 662
+                 printChar('d');
+                 GPIOF->DATA = (1<<3) | (1<<2);
+                 break;
+             default:
+                 printChar((char)valor);
+                 GPIOF->DATA = (0<<1) | (0<<2) | (0<<3);
+                 break;
+         }
+    }
+    */
+    
+    
+    
     while (1)
     {
         int i = 0;
